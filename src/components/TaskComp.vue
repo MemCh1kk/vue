760652982cbae51bd08e035baf127026d414215d
MemCh1kk <memetov.k.i.23@gmail.com>
2025-02-06 12:53:24 +0300

    task 1

---
+++ src/components/TaskComp.vue
@@ -0,0 +1,14 @@
+
+<script>
+	export default{ data() {
+	return {
+		text1: 'Kemal',
+		text2: 'Memetov',
+	}
+    }
+}
+</script>
+<template>
+    <div><p>{{ text1 }}</p></div>
+    <div><p>{{ text2 }}</p></div>
+</template>
